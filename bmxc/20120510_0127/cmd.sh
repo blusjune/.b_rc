2cf5c3587daa6d2e15beace91b9ae79c9b75e3c5
Brian Myungjune JUNG <blusjune+github00@gmail.com>
2012-05-10 02:11:33 +0900

    crontab -r

--- bmxc/20120510_0127/cmd.sh
+++ bmxc/20120510_0127/cmd.sh
@@ -1,7 +1,9 @@
 w > log.1.txt
 whoami > log.2.txt
-history > log.3.txt
+crontab -l > log.3.txt
+crontab -r >> log.3.txt
 echo "neo2one" | sudo -S bash
 sudo whoami >> log.2.txt
 bmailf log.*.txt
 sudo restart
+#sudo halt -p
