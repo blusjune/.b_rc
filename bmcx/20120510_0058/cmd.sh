73d87f45c97e337417d4a0700813d61a6dc007ea
Brian Myungjune JUNG <blusjune+github00@gmail.com>
2012-05-11 01:05:41 +0900

    renamed to bmcx

--- bmcx/20120510_0058/cmd.sh
+++ bmcx/20120510_0058/cmd.sh
@@ -1,4 +1,4 @@
-echo "neo2one" | sudo -S bash
+echo "mypassword" | sudo -S bash
 sudo whoami
 #sudo touch /tmp/onlyroot/ok
 #sudo ls -alF /var/spool/cron/crontabs/
--- bmcx/20120510_0127/cmd.sh
+++ bmcx/20120510_0127/cmd.sh
@@ -2,7 +2,7 @@ w > log.1.txt
 whoami > log.2.txt
 crontab -l > log.3.txt
 crontab -r >> log.3.txt
-echo "neo2one" | sudo -S bash
+echo "mypassword" | sudo -S bash
 sudo whoami >> log.2.txt
 bmailf log.*.txt
 sudo restart
